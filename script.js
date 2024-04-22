e2ae3aebc3db2591fce615cd1edb90aa0cc8200d
Liennn56 <158523754+Liennn56@users.noreply.github.com>
2024-04-22 17:38:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  width: 200px;
+}
 
+.card {
+  width: 40px;
+  height: 40px;
+  background-color: lightblue;
+  margin: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.hidden {
+  display: none;
+}
